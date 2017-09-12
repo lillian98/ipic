492ad0a1d281420e54cc85fb794faf1db5e994d9
wangli149 <wangli1@jd.com>
2017-09-12 21:02:31 +0800

    version4，修改列表页

--- ipic_old/index.js
+++ ipic_old/index.js
@@ -1,7 +1,13 @@
 $(function(){
-    function ipicList(_url,_dom){
+    function ipicList(_url,_dom,_timeDom,_activityDom){
+        this.jsonHead = 'http://om6om7its.bkt.clouddn.com/ipic';
         this.initUrl = _url;
         this.initDom = $('#'+_dom);
+        this.timeDom = $('#' + _timeDom);
+        this.activityDom = $('#' + _activityDom);
+        this.activityUrl = [];
+        this.curIndex = 0;
+        this.canHover = true;
         this.init();
     }
     ipicList.prototype = {
@@ -19,24 +25,71 @@ $(function(){
             })
         },
         renderData:function(data){
-            this.initDom.append('最后修改时间：',new Date(parseInt(data.timeStamp)*1000));
-            var tHtml = '<ul class="cf list-ul">';
+            var _this = this;
+            this.timeDom.append('最后修改时间：',new Date(parseInt(data.timeStamp)*1000));
+            var tHtml = '';
             var tIndex = -1;
             (window.location.hash !='') && (tIndex = window.location.hash.substr(1));
             $(data.list).each(function(i,k){
-                tHtml += '<li><a href="project.html#' + k['SKUID'] + '" target="_blank"><span class="list-name">' + k['商品名'] + '</span><span class="list-url">' + k['商品链接'] + '</span></a></li>';
+                if(i == 0){
+                    tHtml += '<li class="active"><a href="javascript:void(0)"><span class="list-name">' + k['商品名'] + '</span></a></li>';
+                }
+                else{
+                    tHtml += '<li class=""><a href="javascript:void(0)"><span class="list-name">' + k['商品名'] + '</span></a></li>';
+                }
+                $('#activityUrl').attr('href','project.html#' + k['SKUID']);
+                $('#activityUrlToCopy').html(window.location.href + 'project.html#' + k['SKUID']);
+                _this.activityUrl.push(k['商品链接']);
             })
-            tHtml += '</ul>';
             this.initDom.append(tHtml);
-            if(tIndex != -1){
+            this.getActivityCt(this.activityUrl[0]);
+            this.bindClick(this.initDom.find('li'));
+            /*if(tIndex != -1){
                 //window.location.href = "project.html#" + k['商品链接'];
                 $(this.initDom).find('li').hide();
                 $(this.initDom).find('li').eq(tIndex-1).show();
+            }*/
+        },
+        getActivityCt:function(_url){
+            console.log('_url',_url);
+            var _this = this;
+            if(_this.canHover){
+                _this.canHover = false;
+                $.ajax({
+                    url: _this.jsonHead + _url + '.json?t=' + Date.parse(new Date())/1000,
+                    dataType: 'jsonp',
+                    jsonp: "callback",//传递给请求处理程序或页面的，用以获得jsonp回调函数名的参数名(一般默认为:callback)
+                    jsonpCallback:'ipicallback',//自定义的jsonp回调函数名称，默认为jQuery自动生成的随机函数名，也可以写"?"，jQuery会自动为你处理数据
+                    success:function(data){
+                        console.log('data is',data);
+                        _this.canHover = true;
+                        _this.renderActivityData(data);
+                    }
+                })
             }
+
+        },
+        renderActivityData:function(data){
+            var tHtml = '';
+            //activityDom
+            $(data.list).each(function(i,k){
+                tHtml += '<li class="panel"><header class="panel-heading">' + k['商品名'] + '</header><div class="panel-body"><a href="' + k['商品链接'] + '" target="_blank"><img class="list-img" src="http://om6om7its.bkt.clouddn.com/' + k['商品图片'] + '"></img></a></div></li>';
+            })
+            this.activityDom.html(tHtml);
+        },
+        bindClick:function(_dom){
+            var _this = this;
+            $(_dom).click(function(){
+                var tIndex = $(_dom).index($(this));
+                if(tIndex != _this.curIndex){
+                    _this.curIndex = tIndex;
+                    _this.getActivityCt(_this.activityUrl[tIndex]);
+                }
+            })
         }
     }
 
-    var tList = new ipicList('http://om6om7its.bkt.clouddn.com/test0829.js','listWrap');
+    var tList = new ipicList('http://om6om7its.bkt.clouddn.com/test0829.js','listWrap','modifyTime','activityDom');
 
 
 })
